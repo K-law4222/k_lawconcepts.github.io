20b4bb9e6ced2457904a6203dab24fad3461953e
K-law4222 <109693687+K-law4222@users.noreply.github.com>
2022-07-25 23:02:22 +0300

    Update contact-form-hander.php

--- contact-form-hander.php
+++ contact-form-hander.php
@@ -1,1 +1,17 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
+$email_from = "officialklawphotography@gmail.com";
+$email_subject = "New Form Submission";
+$EMAIL_BODY = "User Name: $name.\n".
+  "User Email: $visitor_email.\n".
+  "User Message: $message.\n";
+
+$to ="officialklawphotography@gmail.com";
+$headers = "From: $email_from\r\n;
+mail($to, $email_subject,$email_body,$headers);
+header("Location:")
+
+?>
